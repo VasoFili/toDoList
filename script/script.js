bdb9b927645b89fa647bd021d2cedb1423a7c17b
Vasiliy Filippov <Filippoff.v@mail.ru>
2020-08-25 23:22:01 +0300

    fix(lesson12): recycleBin3

--- script/script.js
+++ script/script.js
@@ -5,15 +5,13 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-let todoData = [];
+const todoData = [];
 // todoData = localStorage.myToto;
 // console.log(todoData);
 
 // let showToDo = function () {
-todoData = localStorage.myToto;
-todoData = todoData.split(',');
-console.log(typeof todoData);
-console.log(todoData);
+//todoData = JSON.parse(localStorage.myToto);
+// todoData = todoData.split(',');
 // };
 // showToDo();
 
@@ -40,6 +38,7 @@ const render = function () {
       const btnTodoCompleted = li.querySelector('.todo-complete');
       btnTodoCompleted.addEventListener('click', function () {
          item.completed = !item.completed;
+         localStorage.clear();
          render();
       });
 
@@ -47,28 +46,23 @@ const render = function () {
       btnRecycleBin.addEventListener('click', function () {
          let index = todoData.indexOf(item);
          todoData.splice(index, 1);
-         // localStorage.remove(item);
-         localStorage.myToto = JSON.stringify(todoData);
-         console.log(localStorage.myToto);
+         localStorage.clear();
          render();
       });
    });
-   // localStorage.myTodo = JSON.stringify(todoData);
+   localStorage.setItem(todoData, JSON.stringify(todoData));
 };
 
 todoControl.addEventListener('submit', function (event) {
    event.preventDefault();
 
-   const newTodo = {
-      value: headerInput.value,
-      completed: false,
-   }
    if (headerInput.value !== '') {
+      const newTodo = {
+         value: headerInput.value,
+         completed: false,
+      }
       todoData.push(newTodo);
-      // localStorage.myToto = todoData;
-      // localStorage.setItem(headerInput.value, completed);
-      localStorage.myToto = JSON.stringify(todoData);
-      console.log(localStorage.myToto);
+      localStorage.clear();
       headerInput.value = '';
       render();
    };
